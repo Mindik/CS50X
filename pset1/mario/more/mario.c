40c7ba2330a6a11bb7f81a2471cda3efc69b0363
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-26 23:25:07 +0500

    Update mario.c

--- pset1/mario/more/mario.c
+++ pset1/mario/more/mario.c
@@ -7,24 +7,28 @@ int main(void)
 
     do
     {
+        // Request an integer from 1 to 8
         n = get_int("Enter positive number: ");
     }
     while (n < 1 || n > 8);
-
+    
+    // Height Cycle
     for (int i = 0; i < n; i++)
-    {
+    {   
+        // printf " " n - i - 1 (1. 8 - 0 - 1 = 7)
         for (int b = 0; b < n - i - 1; b++)
         {
             printf(" ");
 
         }
+        // printf "#" i + 1 (1. 0 + 1 = 1)
         for (int a = 0; a < i + 1; a++)
         {
             printf("#");
         }
-
+        // Prints once "  "
         printf("  ");
-
+        // printf "#" i + 1 (1. 0 + 1 = 1)
         for (int a = 0; a < i + 1; a++)
         {
             printf("#");
