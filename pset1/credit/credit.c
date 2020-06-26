4d93184727bc3490555b5fd95d9fbafe2bad2c9c
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-26 23:43:13 +0500

    Update credit.c

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -14,19 +14,21 @@ int main(void)
 
     do
     {
+        // Request card number >= 0
         number_card = get_long("Enter number card: ");
     }
     while (number_card <= 0);
-
+    // Variable for counting the number of digits
     long number_card_count = number_card;
+    // Variable for calculating the amount
     long number_card_sum_one = number_card;
-
+    // Counts the number of digits
     while (number_card_count != 0)
     {
         number_card_count /= 10;
         count++;
     }
-
+    // Finds the sum of the odd digits from the end and the sum of the products of even digits
     while (number_card_sum_one != 0)
     {
         sum_one += number_card_sum_one % 10;
@@ -36,14 +38,15 @@ int main(void)
         number_card_sum_one /= 10;
     }
     all_sum = (sum_one + sum_two) % 10;
-
+    
     long a_number_card = number_card;
+    // We get the last 2 digits of the card number
     while (a_number_card > 99)
     {
         a_number_card /= 10;
         a = a_number_card;
     }
-
+    // Prints if the condition is met.
     if (a / 10 == 4 && (count == 13 || count == 16) && all_sum == 0)
     {
         printf("VISA\n");
