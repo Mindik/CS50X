46ee31fb0823d41e75f381aeb2b0dea586707290
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-26 23:30:12 +0500

    Update cash.c

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -8,16 +8,14 @@ int main(void)
     int m25, m10, m5, m1, sum = 0;
 
     do
-    {
+    {   
+        // Request amount entry > 0
         dollars = get_float("Change owed: ");
     }
     while (dollars < 0);
-
+    // Convert to cents
     int cents = round(dollars * 100);
-
-    printf("%i\n", cents);
-
-
+    // Get the amount using "/" and "%"
     sum = cents / 25 + cents % 25 / 10 + cents % 25 % 10 / 5 + cents % 25 % 10 % 5 / 1;
 
     printf("%i\n", sum);
