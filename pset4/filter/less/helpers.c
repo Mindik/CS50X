16d6693348e9f924cfa4b51667cff4676e8124b4
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-27 23:54:04 +0500

    Update helpers.c

--- pset4/filter/less/helpers.c
+++ pset4/filter/less/helpers.c
@@ -5,7 +5,8 @@
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
-{
+{   
+    // We go through each pixel
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
@@ -13,9 +14,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float r = image[h][w].rgbtRed;
             float g = image[h][w].rgbtGreen;
             float b = image[h][w].rgbtBlue;
-
+            // Find the average value for each pixel
             int mid = round((b + g + r) / 3.0);
-
+            // Assigned a new value for up shades
             image[h][w].rgbtRed = mid;
             image[h][w].rgbtGreen = mid;
             image[h][w].rgbtBlue = mid;
@@ -34,10 +35,11 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float b = image[h][w].rgbtBlue;
             float g = image[h][w].rgbtGreen;
             float r = image[h][w].rgbtRed;
+            // Formula from specification
             int sepiaRed = round(.393 * r + .769 * g + .189 * b);
             int sepiaGreen = round(.349 * r + .686 * g + .168 * b);
             int sepiaBlue = round(.272 * r + .534 * g + .131 * b);
-
+            // if >= 255 then = 255
             if (sepiaRed >= 255)
             {
                 sepiaRed = 255;
@@ -64,8 +66,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int h = 0; h < height; h++)
     {
+        // Walk only half the width of the row
         for (int w = 0; w < width / 2; w++)
-        {
+        {   
+            // Move mirror using tmp
             RGBTRIPLE tmp = image[h][w];
             image[h][w] = image[h][width - w - 1];
             image[h][width - w - 1] = tmp;
@@ -76,9 +80,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
-{
+{   
+    // Temporary array
     RGBTRIPLE newPixel[height][width];
-
+    // We go through the whole image
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
@@ -87,8 +92,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             float gm = 0.0;
             float bm = 0.0;
             int count = 0;
+            // Use the current row, the row before the current and after
             for (int f = h - 1; f <= h + 1; f++)
-            {
+            {   
+                // If in the limit of the first and last row
                 if (f >= 0 && f < height)
                 {
                     for (int d = w - 1; d <= w + 1; d++)
@@ -111,6 +118,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int mid_r = round(rm / count);
             int mid_g = round(gm / count);
             int mid_b = round(bm / count);
+            // Write new values to a temporary array
             newPixel[h][w].rgbtRed = mid_r;
             newPixel[h][w].rgbtGreen = mid_g;
             newPixel[h][w].rgbtBlue = mid_b;
@@ -120,6 +128,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; w++)
         {
+            // Update old array
             image[h][w].rgbtRed = newPixel[h][w].rgbtRed;
             image[h][w].rgbtGreen = newPixel[h][w].rgbtGreen;
             image[h][w].rgbtBlue = newPixel[h][w].rgbtBlue;
