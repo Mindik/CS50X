09954593ce090ed78d58e8103efd33be7b75960a
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-27 20:22:09 +0500

    Update readability.c

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -11,25 +11,28 @@ int main(void)
     float sentence = 0;
     float L = 0;
     float S = 0;
-
+    // Request text
     string text = get_string("Text: ");
-
+    // Go through all the characters
     for (int b = 0, n = strlen(text); b < n; b++)
-    {
+    {   
+        // If the character is a letter, then increase the counter
         if (isalpha(text[b]))
         {
             letter++;
         }
+        // If the character is " ", then increase the counter
         if (text[b] == ' ')
         {
             word++;
         }
+        // If the character is ".", "!", "?", then increase the counter
         if (text[b] == '.' || text[b] == '!' || text[b] == '?')
         {
             sentence++;
         }
     }
-
+    // Formulas in the specification
     L = letter * 100 / word;
     S = sentence * 100 / word;
 
