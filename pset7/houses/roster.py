0f0f0f099da253144f1990f8a838f4df693e64d5
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-28 23:44:54 +0500

    Update roster.py

--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -2,15 +2,15 @@ import cs50
 import csv
 from sys import argv, exit
 
-
+# Checking the Number of Arguments
 if len(argv) != 2:
     print("Usage: python roster.py 'house'")
     exit(1)
 
 db = cs50.SQL("sqlite:///students.db")
-
+# Query from the database for the entered house
 houses = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[1])
-
+# Print result
 for row in houses:
     if row["middle"] == None:
         print(f"{row['first']} {row['last']}, born {row['birth']}")
