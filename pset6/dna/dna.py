73a9cc1859fc363c5c281e1ab902a8cd5762bee6
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-28 23:25:35 +0500

    Update dna.py

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -2,40 +2,54 @@ from sys import argv
 import csv
 import re
 
-
+# Checking the number of command line arguments
 if len(argv) != 3:
     print("Usage: python dna.py data.csv sequence.txt")
-
+# Open file for read
 with open(argv[2], 'r') as ff:
     sequence = ff.read()
-
+# Open file 'rt'
 with open(argv[1]) as f:
+    # Save dictionary to variable
     data = csv.DictReader(f)
+    # Field names as keys
     key = data.fieldnames
+    # Boolean variable
     check = False
+    # Check every line in the dictionary
     for row in data:
-        for q in range(1, len(key)):
+        # We go through all the keys except the first, as it is the "name"
+        for indexKey in range(1, len(key)):
             if check == True:
                 break
-            one = int(len(re.findall(key[q], sequence)))
-            if one == 1:
-                t = one
-                r = int(row[key[q]])
-                if t != r:
+            # Find the total number of matches
+            allKeyInSequence = int(len(re.findall(key[indexKey], sequence)))
+            # Just one key match, then immediately check the quantity from the dictionary
+            if allKeyInSequence == 1:
+                # Number of matches in the dictionary
+                dictionaryMatches = int(row[key[indexKey]])
+                # If not equal, then exit the cycle
+                if allKeyInSequence != dictionaryMatches:
                     check = False
                     break
-                if q == len(key) - 1 and t == r:
+                # If they are equal and this is the last key, type the name and exit the loop
+                if indexKey == len(key) - 1 and allKeyInSequence == dictionaryMatches:
                     check = True
                     print(row["name"])
                     break
-            if one > 1:
-                pat = f'(?<={key[q]}){key[q]}(?={key[q]})'
-                t = int(len(re.findall(pat, sequence)) + 2)
-                r = int(row[key[q]])
-                if t != r:
+            # If all key > 1
+            if allKeyInSequence > 1:
+                # Save regular expression
+                regularExpression = f'(?<={key[indexKey]}){key[indexKey]}(?={key[indexKey]})'
+                # Find all matches standing together. +2 - start and end
+                sequenceLength = int(len(re.findall(regularExpression, sequence)) + 2)
+                dictionaryMatches = int(row[key[indexKey]])
+                # If not equal, then exit the cycle
+                if sequenceLength != dictionaryMatches:
                     check = False
                     break
-                if q == len(key) - 1 and t == r:
+                # If they are equal and this is the last key, type the name and exit the loop
+                if indexKey == len(key) - 1 and sequenceLength == dictionaryMatches:
                     check = True
                     print(row["name"])
                     break
