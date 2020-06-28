524a238e8a7c27f3c3b6c031e9f740547a9c7dbd
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-28 22:14:30 +0500

    Update readability.py

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -1,12 +1,12 @@
 from cs50 import get_string
 
-
+# Variables for letters, words and sentences
 letter = 0
 word = 1
 sentence = 0
-
-
+# Request text
 text = get_string("Text: ")
+# Loop through every character in the text
 for a in range(len(text)):
     if text[a].isalpha():
         letter += 1
@@ -14,7 +14,7 @@ for a in range(len(text)):
         word += 1
     if text[a] == '.' or text[a] == '!' or text[a] == '?':
         sentence += 1
-
+# Formula from specification
 index = int(round(0.0588 * letter * 100 / word - 0.296 * sentence * 100 / word - 15.8))
 
 if index > 16:
