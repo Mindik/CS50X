809642154955f98f073c7d29181b2a01af04bcd1
Anton Starenkov <61435238+Mindik@users.noreply.github.com>
2020-06-28 23:41:58 +0500

    Update import.py

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -2,20 +2,21 @@ import cs50
 import csv
 from sys import argv, exit
 
-
+# Checking the Number of Arguments
 if len(argv) != 2:
     print("Usage: python import.py characters.csv")
     exit(1)
 
 db = cs50.SQL("sqlite:///students.db")
-
-
+# Open file for read
 with open(argv[1], "r") as characters:
-
+    # Save dict in reader
     reader = csv.DictReader(characters)
-
+    # We look through each line
     for row in reader:
+        # Separate Names
         n = list(row["name"].split(' '))
+        # Insert in db
         if len(n) == 2:
             db.execute("INSERT INTO students (first, last, house, birth) VALUES (?, ?, ?, ?)",
                        n[0], n[1], row["house"], row["birth"])
